508bc36613ba591353f079716df4a4193af7bb5c
RUBIO RODRIGO COSTA <rubio_costa@estudante.sesisenai.org.br>
2023-10-02 21:18:40 -0300

    final node

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const  useRouter = require('./router/userRouter');
+const useRouter = require('./router/userRouter');
+const authRouter = require('./router/authRouter');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3500;
 
+app.use(bodyParser.json());
+
 app.use('/api', useRouter);
+app.use('/api', authRouter);
 
-app.get('/', (req, res) => {
-
-    res.send({
-        nome: "João",
-        telefone: "47 99999-9999"
-    });
-    
-});
+//app.use(cors);
 
 app.listen(port, () => {
     console.log("server online!");
--- nodejs/src/router/userRouter.js
+++ nodejs/src/router/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userControler');
+const authController = require('../controller/authController')
 
-router.post('/users', userController.CreateUser);
-router.get('/users', userController.GetAllUsers);
-router.get('/users/:id', userController.GetUserById);
-router.put('/users/:id', userController.UpdateUser);
-router.delete('/users/:id', userController.DeleteUser);
+router.post('/users', authController.VerifyJWT, userController.CreateUser);
+router.get('/users', authController.VerifyJWT, userController.GetAllUsers);
+router.get('/users/:id', authController.VerifyJWT, userController.GetUserById);
+router.put('/users/:id', authController.VerifyJWT, userController.UpdateUser);
+router.delete('/users/:id', authController.VerifyJWT, userController.DeleteUser);
 
 module.exports = router;
